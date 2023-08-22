f08ca673e6ee68f3dfcd7f02ae0857a77b3cefbd
lixxce5017 <lixxce5017@gmail.com>
2023-08-22 09:59:48 +0900

    첫번째 메뉴에 걸려서 전메뉴 안나오는거 수정

--- CBNU_Kiosk_main/config/settings.py
+++ CBNU_Kiosk_main/config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- CBNU_Kiosk_main/config/urls.py
+++ CBNU_Kiosk_main/config/urls.py
@@ -16,16 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bugger/', include('bugger.urls')),
-    path('', include('bugger.urls'))
+    path('', include('bugger.urls')),
 ]
 
 
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
